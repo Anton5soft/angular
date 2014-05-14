99689ecbf8b3e4696a2514247ba8a7919b935363
Anton Scherbinin <anton@5-soft.com>
2014-05-15 01:55:39 +0300

    new

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,24 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+  .controller('Login', ['$scope','$location','AuthService', function($scope,$location,AuthService) {
+$scope.credentials = {login:'',password: ''};
+        $scope.login = function () {
+            if(AuthService.auth($scope.credentials) ) {
+                $location.path('userlist');
+            }
+            else {
+                $scope.errorMsg = "Auth error use correct Login or Password"
 
+            }
+        }
   }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
-
+  .controller('UserList', ['$scope', function($scope) {
+        $scope.userlist = [{
+            name: "Beatles",
+            songs: ["Yellow Submarine", "Helter Skelter", "Lucy in the Sky with Diamonds"]
+        }, {
+            name: "Rolling Stones",
+            songs: ["Ruby Tuesday", "Satisfaction", "Jumpin' Jack Flash"]
+        }];
   }]);
--- app/js/services.js
+++ app/js/services.js
@@ -6,4 +6,15 @@
 // Demonstrate how to register services
 // In this case it is a simple value service.
 angular.module('myApp.services', []).
-  value('version', '0.1');
+  value('version', '0.1')
+.factory('AuthService', function() {
+    function auth (credentials) {
+        if (credentials.login === 'admin' && credentials.password ==='admin' ) {
+            return true;
+        }
+        return false;
+    }
+       return {
+           auth:auth
+       }
+});
